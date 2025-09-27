34e586bea120836018224f47d3d40e65577f10a8
Aniketjadhav100 <115538535+Aniketjadhav100@users.noreply.github.com>
2025-09-27 10:46:03 +0530

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About() {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>
+        Hi, I'm Aniket! A Cloud DevOps Engineer and React Developer passionate
+        about building scalable apps and automating workflows.
+      </p>
+    </section>
+  );
+}
+
+export default About;

703bf7cf145117dfd3094310524392c8080a946e
Aniketjadhav100 <115538535+Aniketjadhav100@users.noreply.github.com>
2025-09-27 10:45:23 +0530

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>My Portfolio</h1>
+      <p>React Developer | Cloud DevOps | Problem Solver</p>
+    </header>
+  );
+}
+
+export default Header;

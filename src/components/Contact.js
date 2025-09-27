613eceeceb40a1ac4133178f132f43323c045c19
Aniketjadhav100 <115538535+Aniketjadhav100@users.noreply.github.com>
2025-09-27 10:47:07 +0530

    Create Contact.js

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <section>
+      <h2>Contact</h2>
+      <p>Email: aniket@example.com</p>
+      <p>GitHub: github.com/Aniketjadhav100</p>
+    </section>
+  );
+}
+
+export default Contact;
